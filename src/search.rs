077410055e7fbf94cdc247851c60be0dcb3fee04
Dan Nguyen <pedantic@gmail.com>
2021-11-05 12:52:43 -0500

    chore: Run cargo fmt

--- src/search.rs
+++ src/search.rs
@@ -2,69 +2,92 @@ use crate::machine::Instruction;
 use crate::State;
 
 pub struct Schema<'a> {
-	live_in: Vec<Box<dyn for<'r> Fn(&'r mut State, i8)>>,
-	live_out: Vec<Box<dyn for<'r> Fn(&'r State) -> Option<i8> + 'a >>,
+    live_in: Vec<Box<dyn for<'r> Fn(&'r mut State, i8)>>,
+    live_out: Vec<Box<dyn for<'r> Fn(&'r State) -> Option<i8> + 'a>>,
 }
 
 impl<'a> Schema<'_> {
-	pub fn new(live_in: Vec<Box<dyn for<'r> Fn(&'r mut State, i8)>>, live_out: Vec<Box<dyn for<'r> Fn(&'r State) -> Option<i8> + 'a >>) -> Schema {
-		Schema { live_in, live_out }
-	}
+    pub fn new(
+        live_in: Vec<Box<dyn for<'r> Fn(&'r mut State, i8)>>,
+        live_out: Vec<Box<dyn for<'r> Fn(&'r State) -> Option<i8> + 'a>>,
+    ) -> Schema {
+        Schema { live_in, live_out }
+    }
 }
 
 fn run_program(prog: &Vec<Instruction>, schema: &Schema, inputs: &Vec<i8>) -> Option<State> {
-	let mut s = State::new();
-	
-	for (func, val) in schema.live_in.iter().zip(inputs) {
-		(func)(&mut s, *val);
-	}
-	if prog.iter().fold(true, |valid: bool, i| valid && (i.operation)(i, &mut s)) {
+    let mut s = State::new();
+
+    for (func, val) in schema.live_in.iter().zip(inputs) {
+        (func)(&mut s, *val);
+    }
+    if prog
+        .iter()
+        .fold(true, |valid: bool, i| valid && (i.operation)(i, &mut s))
+    {
         Some(s)
     } else {
         None
     }
 }
 
-pub fn equivalence(prog: &Vec<Instruction>, schema: &Schema, test_cases: &Vec<(Vec<i8>, Vec<i8>)>) -> bool {
-	for tc in test_cases {
-		if let Some(state) = run_program(prog, schema, &tc.0) {
-			for (func, val) in schema.live_out.iter().zip(&tc.1) {
-				let result = func(&state);
-				if result != Some(*val) {
-					return false;
-				}
-			}
-		} else {
-			return false;
-		}
-	}
-	return true;
+pub fn equivalence(
+    prog: &Vec<Instruction>,
+    schema: &Schema,
+    test_cases: &Vec<(Vec<i8>, Vec<i8>)>,
+) -> bool {
+    for tc in test_cases {
+        if let Some(state) = run_program(prog, schema, &tc.0) {
+            for (func, val) in schema.live_out.iter().zip(&tc.1) {
+                let result = func(&state);
+                if result != Some(*val) {
+                    return false;
+                }
+            }
+        } else {
+            return false;
+        }
+    }
+    return true;
 }
 
-pub fn exhaustive_search(found_it: &dyn Fn(&Vec<Instruction>) -> bool, instructions: Vec<Instruction>, constants: Vec<i8>, vars: Vec<u16>) {
-    let instrs = instructions.iter().map(|i| i.vectorize(&constants, &vars)).flatten().collect();
+pub fn exhaustive_search(
+    found_it: &dyn Fn(&Vec<Instruction>) -> bool,
+    instructions: Vec<Instruction>,
+    constants: Vec<i8>,
+    vars: Vec<u16>,
+) {
+    let instrs = instructions
+        .iter()
+        .map(|i| i.vectorize(&constants, &vars))
+        .flatten()
+        .collect();
 
-	fn try_all(term: &dyn Fn(&Vec<Instruction>) -> bool, prog: Vec<Instruction>, instrs: &Vec<Instruction>, len: u32) -> bool {
-		if len == 0 {
-			return term(&prog);
-		} else {
-			for ins in instrs {
-				let p = prog.iter().cloned().chain(vec![*ins]).collect();
-				if try_all(term, p, &instrs, len-1) {
-					return true;
-				}
-			}
-			return false;
-		}
-	}
+    fn try_all(
+        term: &dyn Fn(&Vec<Instruction>) -> bool,
+        prog: Vec<Instruction>,
+        instrs: &Vec<Instruction>,
+        len: u32,
+    ) -> bool {
+        if len == 0 {
+            return term(&prog);
+        } else {
+            for ins in instrs {
+                let p = prog.iter().cloned().chain(vec![*ins]).collect();
+                if try_all(term, p, &instrs, len - 1) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 
-	let t: &dyn Fn(&Vec<Instruction>) -> bool = &|v| -> bool { found_it(v) };
+    let t: &dyn Fn(&Vec<Instruction>) -> bool = &|v| -> bool { found_it(v) };
 
-	for i in 1..10 {
-		println!("Trying programs of length {}.", i);
-		if try_all(&t, Vec::new(), &instrs, i){
-			return;
-		}
-	}
-
+    for i in 1..10 {
+        println!("Trying programs of length {}.", i);
+        if try_all(&t, Vec::new(), &instrs, i) {
+            return;
+        }
+    }
 }
